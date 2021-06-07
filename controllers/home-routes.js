f236e7d387f955e1f0c237f4abca9d6739ec5192
Lucas Q <lucas.14.96@gmail.com>
2021-06-07 10:38:11 -0500

    Added sample posts, modified routes and added post add/delete/edit functions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,6 +41,58 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'post_content',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: [
+          'id',
+          'comment_content',
+          'post_id',
+          'user_id',
+          'created_at'
+        ],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No Post found with this id' });
+      return;
+    }
+    //serialize the data
+    const post = dbPostData.get({ plain: true });
+
+    //pass data to the template
+    res.render('single-post', {
+      post, 
+      loggedIn: req.session.loggedIn
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
